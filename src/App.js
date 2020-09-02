b665cde340a4087c77859f70d02ddf8c4af1001f
ben <noahbenaas@gmail.com>
2020-09-02 14:17:49 +0200

    test

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+      <div className= "container">
+        <h2>CSS Module</h2>
+        <div className= " form-container">
+        <div>
+          <h3>Rules</h3>
+          <p>lorem ved lanseringen av Letraset-ark med avsnitt fra Lorem Ipsum, og senere med sideombrekkings</p>
+          </div>
+          <div>
+          <form className= "form">
+          <h3>Register</h3>
+          <input placeholder= "NAME" />
+          <input placeholder= "CITY" />
+          <button>Register</button>
+          <button>Clear</button>
+          </form>
+        </div>
+
+        <div className="col-login">
+        <button className="login" />
+</div>
+<div>
+  <h3>TERM</h3>
+  <p className="terms">
+    lorem ved lanseringen av Letraset-ark med avsnitt fra Lorem Ipsum, og senere med sideombrekkings
+  </p>
+</div>
+</div>
+</div>
+
+
+
+
+       
+
+  );
+}
+
+export default App;
